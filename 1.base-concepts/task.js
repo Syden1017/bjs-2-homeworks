07273a7c6f31f2fa3f165b0641e5501e5166820d
Alex <sasha.shustov.05@mail.ru>
2024-08-29 19:03:16 +0300

    Second task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -22,5 +22,12 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-
+  const monthlyRate = (percent / 100) / 12;
+  const loanBody = amount - contribution;
+  if (loanBody === 0) {
+    return parseFloat(loanBody.toFixed(2));
+  }
+  const payment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+  const totalPayment = payment * countMonths;
+  return parseFloat(totalPayment.toFixed(2));
 }
